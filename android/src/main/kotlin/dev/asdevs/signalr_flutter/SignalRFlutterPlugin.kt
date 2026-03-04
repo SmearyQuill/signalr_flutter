67497154b22d4ffeab11c531125cc5d4ac116b28
Daniel De Luna <35876982+SmearyQuill@users.noreply.github.com>
2026-03-04 14:18:07 -0600

    Update SignalRFlutterPlugin.kt

--- android/src/main/kotlin/dev/asdevs/signalr_flutter/SignalRFlutterPlugin.kt
+++ android/src/main/kotlin/dev/asdevs/signalr_flutter/SignalRFlutterPlugin.kt
@@ -61,7 +61,7 @@ class SignalrFlutterPlugin : FlutterPlugin, SignalrApi.SignalRHostApi {
                     Handler(Looper.getMainLooper()).post {
                         signalrApi.onNewMessage(methodName, res) { }
                     }
-                }, String::class.java)
+                },  dynamic::class.java)
             }
 
             connection.connected {
